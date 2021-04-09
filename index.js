33cf8c1a510396214dc760d70f6d1fd247615168
eman hamid <emanhharki@gmail.com>
2021-04-09 14:17:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ function moveDodgerRight() {
   let right = parseInt(rightNumbers, 10);
 
   if (right > 0) {
-    dodger.style.left = `${left - 1}px`;
+    dodger.style. = `${left - 1}px`;
   }
 }
