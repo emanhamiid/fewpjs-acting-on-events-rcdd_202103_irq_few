1387979aa1ead97d2a263b9ca45aa4ffecf92c9b
eman hamid <emanhharki@gmail.com>
2021-04-09 14:17:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function moveDodgerLeft() {
   }
 }
 function moveDodgerRight() {
-  let leftNumbers = dodger.style.left.replace("px", "");
+  let lNumbers = dodger.style.left.replace("px", "");
   let left = parseInt(leftNumbers, 10);
 
   if (left > 0) {
